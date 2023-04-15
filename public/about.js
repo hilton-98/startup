78fc308c2a62dc0de55035425dc957d2c0cbe8f4
Kevin Hilton <hiltonk98@gmail.com>
2023-04-15 13:30:38 -0600

    Added calling a third-party service

--- public/about.js
+++ public/about.js
@@ -9,10 +9,32 @@ const mainEl = bodyEl.querySelector('main');
 const sidebarEl = mainEl.querySelector('.sidebar');
 const footerEl = bodyEl.querySelector('footer');
 
+function displayQuote(data) {
+
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.getElementById('quote-container');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+      quoteEl.textContent = `"${data.content}"`;
+        authorEl.textContent = `--${data.author}`;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+}
+
+
 function init() {
     addHeader(headerEl, localStorage.getItem('username'));
     addSidebar(sidebarEl, "About");
     addFooter(footerEl);
+    displayQuote();
 }
 
 init();
