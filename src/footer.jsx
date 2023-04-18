184d92a22e5ed376aab9bd5524b90be52f950f73
Kevin Hilton <hiltonk98@gmail.com>
2023-04-18 10:21:58 -0600

    Fixed issues with CSS styling

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,5 +1,6 @@
 import react from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css'
 
 export function Footer() {
--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -1,33 +1,45 @@
-import './sidebar.css';
+import react from 'react';
 
-const navLinkList = [
-    { name: "Home", path: "./index.html" },
-    { name: "To Do", path: "./toDo.html" },
-    { name: "Calendar", path: "./calendar.html" },
-    { name: "About", path: "./about.html" },
-]
+import './sidebar.css';
 
 
 export function Sidebar(currPageName) {
     
+    const navLinkList = [
+        { name: "Home", path: "./index.html" },
+        { name: "To Do", path: "./toDo.html" },
+        { name: "Calendar", path: "./calendar.html" },
+        { name: "About", path: "./about.html" },
+    ];
+
+
+    let path = window.location.pathname;
+    let pageName = path.split("/").pop();
+
+    const navLinksEl = [];
+    for (let i = 0; i < navLinkList.length; i++) {
+
+        const navLink = navLinkList[i];
+
+        let className = 'nav-link';
+        if (navLink.path.includes(pageName)) {
+            className += ' current-page-link';
+        }
+
+        navLinksEl.push(
+            <a key={i} className={className} href={navLink.path}>
+                {navLink.name}
+            </a>
+        );
+    }
+
     return (
         <section className="sidebar">
             <h2 className="sidebar-header">
                 Menu
             </h2>
             <div className="nav-links">
-                <a className='nav-link' href="./index.html">
-                    Home
-                </a>
-                <a className='nav-link' href="./index.html">
-                    To Do
-                </a>
-                <a className='nav-link' href="./index.html">
-                    Calendar
-                </a>
-                <a className='nav-link' href="./index.html">
-                    About
-                </a>
+                {navLinksEl}
             </div>
         </section>
     );
